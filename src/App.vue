90e2fffbceba536c72834b95a1509789a933a722
Jordan Romagnoli <146195147+JordanRomagnoli@users.noreply.github.com>
2024-01-18 13:32:43 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script>
+
+/*
+    Importazione componente
+*/
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+    data() {
+        return { 
+             
+        }
+    },
+    /*
+        Dichiarazione dei componenti all'interno dell'oggetto
+    */
+    components: {
+
+        AppHeader,
+        AppMain,
+        AppFooter
+    },
+}
+</script>
+
+<template>
+    <div>
+        <!--
+            Utilizzo del componente
+        -->
+        <AppHeader/>
+        <AppMain/>
+        <AppFooter/>
+        
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
